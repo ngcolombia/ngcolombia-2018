b0a84d8a4375cf930aee37103faa0b49c3c704a3
Leonardo Pérez <leonardo.perez@yuxiglobal.com>
2018-03-10 22:00:38 -0500

    feat: Add all sections with transitions and footer as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from "@angular/service-worker";
-import { MatButtonModule } from '@angular/material'
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
 import { CallForPapersComponent } from './call-for-papers/call-for-papers.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NewsletterSignupComponent } from './newsletter-signup/newsletter-signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CallForPapersComponent
+    CallForPapersComponent,
+    WelcomeComponent,
+    NewsletterSignupComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     ServiceWorkerModule,
-    MatButtonModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
