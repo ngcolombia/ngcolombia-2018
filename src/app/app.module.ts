9808a4cf765a1bbdb780dbf4564dda0a8400e27d
jdjuan <david.juanherrera@gmail.com>
2018-03-23 09:09:05 -0500

    fix: remove toggleSlider and typo on newsletter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatInputModule, MatFormFieldModule, MatSnackBarModule, MatSliderModule } from '@angular/material';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { CallForPapersComponent } from './call-for-papers/call-for-papers.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { NewsletterSignupComponent } from './newsletter-signup/newsletter-signup.component';
-import { FooterComponent } from './footer/footer.component';
-
 import { environment } from '../environments/environment';
-import { ToggleSliderComponent } from './welcome/toggle-slider/toggle-slider.component';
-import { ParallaxHoverDirective } from './shared/directives/parallax-hover.directive';
-import { ParallaxBigBangDirective } from './shared/directives/parallax-bigbang.directive';
+import { FooterComponent } from './footer/footer.component';
 import { MadnessDirective } from './shared/directives/madness.directive';
+import { NewsletterSignupComponent } from './newsletter-signup/newsletter-signup.component';
+import { NgModule } from '@angular/core';
+import { ParallaxBigBangDirective } from './shared/directives/parallax-bigbang.directive';
+import { ParallaxHoverDirective } from './shared/directives/parallax-hover.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSnackBarModule,
+  MatSliderModule,
+} from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
@@ -22,7 +27,6 @@ import { MadnessDirective } from './shared/directives/madness.directive';
     FooterComponent,
     WelcomeComponent,
     MadnessDirective,
-    ToggleSliderComponent,
     ParallaxHoverDirective,
     CallForPapersComponent,
     ParallaxBigBangDirective,
@@ -42,6 +46,6 @@ import { MadnessDirective } from './shared/directives/madness.directive';
     MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
