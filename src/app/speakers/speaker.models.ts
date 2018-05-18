31c667c103786695eac2c3a1cbf2719420358c2f
Luchillo <luchillo17@gmail.com>
2018-05-18 00:32:39 -0500

    [Feat] Finish first attempt at Speakers page

---
+++ src/app/speakers/speaker.models.ts
@@ -0,0 +1,8 @@
+export class Speaker {
+  constructor(
+    public name = 'Jhon Doe',
+    public title = 'Google Developer',
+    public bio = 'lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.',
+    public photo = 'https://picsum.photos/600/400?random',
+  ) {}
+}
