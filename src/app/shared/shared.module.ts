8b1ad3e186100793d874fdc6f10564fc747101bd
jdjuan <david.juanherrera@gmail.com>
2018-05-20 13:07:28 -0500

    feat: add sidebar to the right side

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,31 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-
-import { MaterialModule } from '../material/material.module';
+import { FooterComponent } from './footer/footer.component';
 import { MadnessDirective } from './directives/madness.directive';
+import { MaterialModule } from '../material/material.module';
+import { MenuComponent } from './menu/menu.component';
+import { NgModule } from '@angular/core';
 import { ParallaxBigBangDirective } from './directives/parallax-bigbang.directive';
 import { ParallaxHoverDirective } from './directives/parallax-hover.directive';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './header/menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
-  declarations: [
-    FooterComponent,
-    HeaderComponent,
-    MadnessDirective,
-    MenuComponent,
-    ParallaxBigBangDirective,
-    ParallaxHoverDirective,
-  ],
+  imports: [CommonModule, MaterialModule, RouterModule],
+  declarations: [FooterComponent, MadnessDirective, MenuComponent, ParallaxBigBangDirective, ParallaxHoverDirective],
   exports: [
     FooterComponent,
-    HeaderComponent,
     MadnessDirective,
     MaterialModule,
     ParallaxBigBangDirective,
     ParallaxHoverDirective,
+    MenuComponent,
   ],
 })
 export class SharedModule {}
