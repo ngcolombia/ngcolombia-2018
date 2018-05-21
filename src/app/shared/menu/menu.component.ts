1494f4f3d0f3ba270dd59009540c5b223ff1801a
jdjuan <david.juanherrera@gmail.com>
2018-05-21 12:13:28 -0500

    feat: add anitmation fading

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,13 +1,65 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+  } from '@angular/animations';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Directive } from '@angular/core';
+import { delay, map, pairwise } from 'rxjs/operators';
+import { ElementRef } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('scrollAnimation', [
+      state(
+        'show',
+        style({
+          backgroundColor: 'rgba(255, 255, 255, 0.6)',
+        }),
+      ),
+      state(
+        'hide',
+        style({
+          backgroundColor: 'transparent',
+        }),
+      ),
+      transition('show => hide', animate('700ms ease-out')),
+      transition('hide => show', animate('700ms ease-in')),
+      transition('* => hide', animate('700ms ease-in')),
+    ]),
+  ],
 })
-export class MenuComponent {
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
-  constructor(private breakpointObserver: BreakpointObserver) {}
+export class MenuComponent implements OnInit {
+  isHandset: Observable<BreakpointState>;
+  toolbarState: string;
+  scrolling = new Subject();
+  readonly TOOLBAR_DEBOUNCE_TIME = 100;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.scrolling.next(window.pageYOffset);
+  }
+  constructor(private breakpointObserver: BreakpointObserver, private el: ElementRef) {
+    this.isHandset = this.breakpointObserver.observe(Breakpoints.Handset);
+  }
+
+  ngOnInit() {
+    this.scrolling
+      .pipe(
+        delay(this.TOOLBAR_DEBOUNCE_TIME),
+        pairwise(),
+        map(([previousValue, newValue]) => (previousValue >= newValue ? 'show' : 'hide')),
+      )
+      .subscribe(toolbarState => {
+        this.toolbarState = toolbarState;
+      });
+  }
 }
