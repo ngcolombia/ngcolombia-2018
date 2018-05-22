ea5a80b6033258d053a0f09f3e66d2e28bb62ee9
Luchillo <luchillo17@gmail.com>
2018-05-21 22:56:54 -0500

    refactor: Change folder structure & styles for full page title

--- src/app/speakers/core/speaker.model.ts
+++ src/app/speakers/core/speaker.model.ts
@@ -6,6 +6,7 @@ export class Speaker {
     public photo: string = null,
     public twitter: string = null,
     public github: string = null,
+    public website: string = null,
   ) {
     if (this.photo === null) {
       this.photo = `https://picsum.photos/600/400?random&image=${Math.round(Math.random() * 100)}`;
