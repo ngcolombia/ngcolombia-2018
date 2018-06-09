4c5a9f3819a8574cd6f2063f62732d7ae8fd6e21
jdjuan <david.juanherrera@gmail.com>
2018-06-09 09:44:22 -0500

    refactor: move material module inside shared module

--- src/app/speakers/speakers.module.ts
+++ src/app/speakers/speakers.module.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-import { MaterialModule } from '../material/material.module';
 import { SpeakerPhotoUrlPipe } from './core/speaker-photo.pipe';
 import { SpeakersBioComponent } from './speakers-bio/speakers-bio.component';
 import { SpeakersListComponent } from './speakers-list/speakers-list.component';
 import { SpeakersRoutingModule } from './speakers-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, FontAwesomeModule, MaterialModule, SpeakersRoutingModule],
-  declarations: [SpeakerPhotoUrlPipe, SpeakersBioComponent, SpeakersListComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    SharedModule,
+    SpeakersRoutingModule,
+  ],
+  declarations: [
+    SpeakerPhotoUrlPipe,
+    SpeakersBioComponent,
+    SpeakersListComponent,
+  ],
   entryComponents: [SpeakersBioComponent],
 })
 export class SpeakersModule {}
