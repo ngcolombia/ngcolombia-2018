b6d766d61bea66800449305686887d251e2bc3aa
jdjuan <david.juanherrera@gmail.com>
2018-06-09 09:44:22 -0500

    refactor: move material module inside shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-import { MaterialModule } from '../material/material.module';
 import { MadnessDirective } from './directives/madness.directive';
 import { ParallaxBigBangDirective } from './directives/parallax-bigbang.directive';
 import { ParallaxHoverDirective } from './directives/parallax-hover.directive';
 import { ParallaxScrollDirective } from './directives/parallax-scroll.directive';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, RouterModule],
-  declarations: [FooterComponent, MadnessDirective, MenuComponent, ParallaxBigBangDirective, ParallaxScrollDirective, ParallaxHoverDirective],
+  declarations: [
+    FooterComponent,
+    MadnessDirective,
+    MenuComponent,
+    ParallaxBigBangDirective,
+    ParallaxScrollDirective,
+    ParallaxHoverDirective,
+  ],
   exports: [
     FooterComponent,
     MadnessDirective,
