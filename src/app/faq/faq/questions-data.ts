eb5641979a9265a45f74c1bd52a3692af1fb6995
jdjuan <david.juanherrera@gmail.com>
2018-06-25 14:06:44 -0500

    fix: typos on FAQ

--- src/app/faq/faq/questions-data.ts
+++ src/app/faq/faq/questions-data.ts
@@ -2,7 +2,7 @@ export const QUESTIONS = [
   {
     title: '1. What is Angular?',
     description:
-      'Angular is a platform for developing web and mobile applications. It is developed at Google and supported by a worldwide community. Today, Angular is one of the most popular developement technologies.',
+      'Angular is a platform for developing web and mobile applications. It is developed at Google and supported by a worldwide community. Today, Angular is one of the most popular development technologies.',
   },
   {
     title: '2. What speakers are coming?',
@@ -12,46 +12,46 @@ export const QUESTIONS = [
   {
     title: '3. What is the duration of the event?',
     description:
-      'This is a 2-day event. The first day we will have 4 workshops, and in the second day we will have keynotes and talks.',
+      'This is a 2-day event. The first day we will have 4 workshops. The second day we will have keynotes and talks.',
   },
   {
-    title: '4. Can I participate of two workshops?',
-    description:
-      'Yes. Two workshops will be carried out in the morning and the other two in the afternoon. You have to puck a combination that does not collide.',
-  },
-  {
-    title: '5. Is lunch included?',
-    description:
-      'No. Lunch is free choice. Yet, during the event we will provide snacks constantly.',
-  },
-  {
-    title: '6. What is the language of the conference?',
-    description:
-      'The event will be held in English. We will provide translation devices for both the workshops and the conference.',
-  },
-  {
-    title: '7. Are there any discounts in the tickets?',
-    description:
-      'Ticket prices are fixed, nonetheless, Premium and Enterprise bundles have discounts of 14% and 19% respectively',
-  },
-  {
-    title: '8. Do you have diversity scholarships?',
-    description:
-      'Yes. We will have a diversity program to provide underrepresented people the opportunity to join us. More information coming soon.',
-  },
-  {
-    title: '9. Will there be an afterparty?',
-    description:
-      'We will have a cocktail at the end of the conference for you to meet other attendees and speakers.',
-  },
-  {
-    title: '10. Where can I purchase my ticket?',
+    title: '4. Where can I purchase my ticket?',
     description:
       'You can purchase your tickets online with Credit or Debit card (PSE). If you live in Medellin, you can purchase your tickets in shopping centers Oviedo y Unicentro at Almacenes la música (Cash or Card). Additionaly, you can purchase them via phone call with Ticket Express (Credit Card).',
   },
   {
-    title: '11. How many tickets can I purchase per transaction?',
+    title: '5. How many tickets can I purchase per transaction?',
     description:
       'You can only purchase 1 ticket per transaction since it is required that every attendee fill in additional information.',
+  },
+  {
+    title: '6. Can I participate of two workshops?',
+    description:
+      'Yes. Two workshops will be carried out in the morning and the other two in the afternoon. You have to pick a combination that does not collide.',
+  },
+  {
+    title: '7. Is lunch included?',
+    description:
+      'No. Lunch is free choice. Yet, during the event we will provide snacks constantly.',
+  },
+  {
+    title: '8. What is the language of the conference?',
+    description:
+      'The event will be held in English. We will provide translation devices for both, the workshops and the conference.',
+  },
+  {
+    title: '9. Are there any discounts in the tickets?',
+    description:
+      'Ticket prices are fixed, nonetheless, Premium and Enterprise bundles have discounts of 14% and 19% respectively',
+  },
+  {
+    title: '10. Do you have diversity scholarships?',
+    description:
+      'Yes. We will have a diversity program to provide underrepresented people the opportunity to join us. More information coming soon.',
+  },
+  {
+    title: '11. Will there be an afterparty?',
+    description:
+      'We will have a cocktail at the end of the conference for you to meet other attendees and speakers.',
   },
 ];
