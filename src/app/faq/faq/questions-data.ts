46080c09f37e6ab3956f952e980bf456c2357c36
jdjuan <david.juanherrera@gmail.com>
2018-06-26 11:20:18 -0500

    fix: update texts

--- src/app/faq/faq/questions-data.ts
+++ src/app/faq/faq/questions-data.ts
@@ -2,7 +2,7 @@ export const QUESTIONS = [
   {
     title: '1. What is Angular?',
     description:
-      'Angular is a platform for developing web and mobile applications. It is developed at Google and supported by a worldwide community. Today, Angular is one of the most popular development technologies.',
+      'Angular is a platform that makes it easy to build applications with the web. Angular combines declarative templates, dependency injection, end to end tooling, and integrated best practices to solve development challenges. Angular empowers developers to build applications that live on the web, mobile, or the desktop.',
   },
   {
     title: '2. What speakers are coming?',
