b0e0970047812ce5579620d49b0a275c2de7cc94
jdjuan <david.juanherrera@gmail.com>
2018-06-27 15:25:42 -0500

    fix: typos on information

--- src/app/faq/faq/questions-data.ts
+++ src/app/faq/faq/questions-data.ts
@@ -7,46 +7,46 @@ export const QUESTIONS = [
   {
     title: '2. What speakers are coming?',
     description:
-      'NgColombia will have renown speakers coming from Denmark, Poland, USA, England and much more. Many of them are Google Developer Experts. To see the full list take a look at the Speaker section.',
+      'NgColombia will have renown speakers coming from Denmark, Poland, the USA, England and much more. Many of them are Google Developer Experts. To see the full list, take a look at the Speaker section.',
   },
   {
     title: '3. What is the duration of the event?',
     description:
-      'This is a 2-day event. The first day we will have 4 workshops. The second day we will have keynotes and talks.',
+      'This is a 2-day event. On the first day, we will have 4 workshops, on the second one, we will have keynotes and talks.',
   },
   {
     title: '4. Where can I purchase my ticket?',
     description:
-      'You can purchase your tickets online with Credit or Debit card (PSE). If you live in Medellin, you can purchase your tickets in shopping centers Oviedo y Unicentro at Almacenes la música (Cash or Card). Additionaly, you can purchase them via phone call with Ticket Express (Credit Card).',
+      'You can purchase your tickets online with Credit or Debit card (PSE). If you live in Medellin, you can purchase your tickets in shopping centers: Oviedo and Unicentro at Almacenes la música (Cash or Card). Additionaly, you can purchase them via phone call with Ticket Express (Credit Card).',
   },
   {
     title: '5. How many tickets can I purchase per transaction?',
     description:
-      'You can only purchase 1 ticket per transaction since it is required that every attendee fill in additional information.',
+      'You can only purchase 1 ticket per transaction, since it is required that every attendee fill in additional information.',
   },
   {
     title: '6. What information do I need to present?',
     description: 'You only need to present your ticket (digital or printed) and your ID.',
   },
   {
-    title: '7. Can I participate of two workshops?',
+    title: '7. Can I participate in two of the workshops?',
     description:
-      'Yes. Two workshops will be carried out in the morning and the other two in the afternoon. You have to pick a combination that does not collide.',
+      'Yes. Two workshops will be carried out in the morning and the other two in the afternoon. You are able to to pick the combination that does not interfere one another.',
   },
   {
     title: '8. Is lunch included?',
     description:
-      'No. Lunch is free choice. Yet, during the event we will provide snacks constantly.',
+      'No. Lunch is on your own. Though, during the event, we will provide snacks constantly.',
   },
   {
     title: '9. What is the language of the conference?',
     description:
-      'The event will be held in English. We will provide translation devices for both, the workshops and the conference.',
+      'The event will be held in English. We will provide translation devices for either the workshops and conference.',
   },
   {
     title: '10. Are there any discounts in the tickets?',
     description:
-      'Ticket prices are fixed, nonetheless, Premium and Enterprise bundles have discounts of 14% and 19% respectively',
+      'Ticket prices are fixed, nonetheless, Premium and Enterprise bundles have discounts of 14% and 19% respectively.',
   },
   {
     title: '11. Do you have diversity scholarships?',
