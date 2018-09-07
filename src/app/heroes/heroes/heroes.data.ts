479954afb163ee686c4ecd3e1f8bd62b40a30d71
jdjuan <david.juanherrera@gmail.com>
2018-09-07 07:12:17 -0500

    feat: update agenda

--- src/app/heroes/heroes/heroes.data.ts
+++ src/app/heroes/heroes/heroes.data.ts
@@ -3,13 +3,13 @@ export const heroesData = [
     name: 'Juan Herrera',
     picture: './assets/img/heroes/juan.jpg',
     phrase:
-      'He learned that real - world impact doesn\'t come from solo genius, but from collective genius like this one. Thank you all ❤️️.',
+      'He learned that world impact doesn\'t come from solo genius, but from collective genius like this one. Thank you all ❤️️.',
   },
   {
     name: 'Ana Sánchez',
-    picture: './assets/img/heroes/anasanchez.jpg',
+    picture: './assets/img/heroes/ana.jpg',
     phrase:
-      'Her tireless spirity and constant enthusiasm ketp our social media alive. She was the mastermind behind every copy.',
+      'Her tireless spirit and constant enthusiasm ketp our social media alive. She was the mastermind behind every copy.',
   },
   {
     name: 'Camila Cárdenas',
@@ -21,18 +21,11 @@ export const heroesData = [
     name: 'Juliana Aristizábal',
     picture: './assets/img/heroes/juliana.jpg',
     phrase:
-      'Juliana was the brain behind our visual identity, her skills made out website and social media look as it does today.',
-  },
-
-  {
-    name: 'Milena Mora',
-    picture: './assets/img/heroes/milenamora.jpg',
-    phrase:
-      'She created NgColombia’s official video, the one we are proud to show everywhere :).',
+      'Juliana was the brain behind our visual identity, her skills made our website and social media look as it does today.',
   },
   {
     name: 'María Llano',
-    picture: './assets/img/heroes/mariallano.jpg',
+    picture: './assets/img/heroes/mafe.jpg',
     phrase:
       'Mafe\'s is well known for having a great heart, but more than that, she\'s is a very determined woman. From the very beginning she helped us build this.',
   },
@@ -50,7 +43,7 @@ export const heroesData = [
   },
   {
     name: 'Roger Alvarez',
-    picture: './assets/img/heroes/rogeralvarez.jpg',
+    picture: './assets/img/heroes/roger.png',
     phrase:
       'Roger always helped us tackle any technical problem we encounter but also served as mentor in the Unit Tests workshop.',
   },
@@ -58,46 +51,72 @@ export const heroesData = [
     name: 'Juan Rendón',
     picture: './assets/img/heroes/jose.jpg',
     phrase:
-      'Our PWA expert is Jose. He did not only help us enginner our website but also served as mentor for our Firebase workshop. Probably the most humble person we\'ve met',
+      'Our PWA expert is Jose. He did not only help us enginner our website but also served as mentor for our Firebase workshop. Probably the most humble person we\'ve met.',
   },
   {
     name: 'Leonardo Pérez',
     picture: './assets/img/heroes/leo.jpg',
     phrase:
-      'The kind of person that blows your mind on a Tuesday afternoon with a pixel perfect, responsive, and well design website. He\'s got tremendous learning skills, perfect English, and the eye for great design.',
-  },
-  {
-    name: 'Manuela Castrillón',
-    picture: './assets/img/heroes/manuela.jpg',
-    phrase: 'Angular lover that helped us as a tutor for the Workshop #1.',
+      'The kind of person that blows your mind on a Tuesday afternoon with a pixel perfect, responsive, and well designed website. He\'s got tremendous learning skills, perfect English, and the eye for great design.',
   },
   {
     name: 'Marian Villa',
     picture: './assets/img/heroes/marian.jpg',
     phrase:
-      'She helped us designing the art of the NgGirls workshop. She was very supportive.',
+      'She was part of the academic committee and helped us choose the speakers, she shared the word about NgColombia with everyone.',
+  },
+  {
+    name: 'Diana Álvarez',
+    picture: './assets/img/heroes/diana.jpg',
+    phrase:
+      'She was part of the academic committee and helped us choose the speakers, she was always excited about the event!',
   },
   {
     name: 'Carlos Lopez',
     picture: './assets/img/heroes/luchillo.jpg',
     phrase:
-      'He was committed as a tutor for the RxJS workshop. His extensive knowledge was key.',
+      'He was committed as a tutor for the RxJS workshop. His extensive knowledge was key. He was also part of Website Committee.',
   },
   {
-    name: 'Harlen Giraldo',
-    picture: './assets/img/heroes/harlengiraldo.jpg',
-    phrase: 'He was in charge of Angular Medellin twitter. And he did a great job.',
-  },
-  {
-    name: 'Mateo Castaño',
-    picture: './assets/img/heroes/mateocastaño.jpg',
+    name: 'Bram Borggreve',
+    picture: './assets/img/heroes/bram.jpg',
     phrase:
-      'He always supported our community, he helped us by being a tutor in the Workshop #1.',
+      'Bram saved our lives. He helped us prepare a workshop in less than a week. A big man with a big heart.',
   },
   {
-    name: 'Carlos Rojas',
-    picture: './assets/img/heroes/carlosrojas.jpg',
+    name: 'Jorge Vergara',
+    picture: './assets/img/heroes/jorge.jpg',
     phrase:
-      'He gave every NgGirls attendee a 100% off discount for his Angular course in ng-classroom. Plus another 20 for the conference!',
+      'Jorge was part of the academic committee and he was also a mentor for one of our workshops. His constant support was very encouraging!',
+  },
+  {
+    name: 'Manuela Castrillón',
+    picture: './assets/img/heroes/manuela.jpg',
+    phrase:
+      'Manuela helped us pick the speakers and also helped us organize NgGirls! This is her second year joining us.',
+  },
+  {
+    name: 'Vanessa Aristizabal',
+    picture: './assets/img/heroes/vanessa.jpg',
+    phrase:
+      'Vanessa is a very determined person. She helped us organize NgGirls but also participated as speaker.',
+  },
+  {
+    name: 'Alejandra Giraldo',
+    picture: './assets/img/heroes/alejandra.jpg',
+    phrase:
+      'Alejandra was key at the organization of NgGirls, her passion was instilled in all of us.',
+  },
+  {
+    name: 'Laura Ciro',
+    picture: './assets/img/heroes/laura.jpg',
+    phrase:
+      'Laura supported NgColombia from the very beginning, not only by being speaker but also organizing NgGirls.',
+  },
+  {
+    name: 'Kevin Quintero',
+    picture: './assets/img/heroes/kevin.jpg',
+    phrase:
+      'Kevin was essential for every logistic operation, his helpful spirit was key to achieve a smooth execution of NgColombia.',
   },
 ];
